1fa864bc1032916990ac083aadd80d09cc635afe
egemen6k <egemen.altikardesler@gmail.com>
2021-10-15 22:47:44 +0300

    +Challange completed

---
+++ AnimalGame/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float _canSend = -1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canSend)
+        {
+            _canSend = Time.time + 1f;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ AnimalGame/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    void SpawnRandomBall ()
+    {
+        spawnInterval = Random.Range(3,5);
+        Invoke("SpawnRandomBall", spawnInterval);
+
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int randomNumber = Random.Range(0, 3);
+        Instantiate(ballPrefabs[randomNumber], spawnPos, ballPrefabs[randomNumber].transform.rotation);
+    }
+}
